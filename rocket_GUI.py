2a94951bae2e7df536f4f23112d737d55bee9bf9
filete-filete <felixrossiterborg@gmail.com>
2026-03-01 22:43:18 +0100

    Add files via upload

--- rocket_GUI.py
+++ rocket_GUI.py
@@ -13,7 +13,13 @@ stop = False
 
 def session_loop(platform, username):
     # when the start button is pressed, start_session() begins session_loop()
-    print(platform + username)
+    global stop
+    stop = False
+    while not stop:
+        winsound.Beep(700,666)
+        scraper.get_stats(platform, username)
+        time.sleep(60)
+        
 
 def start_session(platform, username):
     t = threading.Thread(target = session_loop, args = (platform, username))
@@ -60,10 +66,13 @@ var2 = tk.IntVar()
 tk.Checkbutton(root, text="Overtime", variable=var1).grid(row=3, column = 0)
 tk.Checkbutton(root, text="Forfeit", variable=var2).grid(row=3, column = 1)
 
-button_s = tk.Button(root, text="START", padx=30, pady=20, bg = "#93fa8c", command = lambda platform = "Epic", username = "filete_filete": scraper.start_session)
+button_s = tk.Button(root, text="START", padx=30, pady=20, bg = "#93fa8c", command = lambda platform = "Epic", username = "filete_filete": start_session(platform, username))
 button_s.grid(row = 4, column= 0)
 
-button_s = tk.Button(root, text = "STOP", padx=30, pady=20, bg = "#fa8c8c", command = scraper.end_session)
+button_s = tk.Button(root, text = "STOP", padx=30, pady=20, bg = "#fa8c8c", command = end_session)
 button_s.grid(row = 4, column = 1)
+
+text = tk.Text(root, height = 10, width = 28)
+text.grid(row = 5, columnspan = 2)
 
 root.mainloop() # starts the event loop and keeps the window responsive
